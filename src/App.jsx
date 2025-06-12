c017c7f81ab7d3260e8526ffc56a028577f2bded
BeshrAlkharrat <beshralkharrat@proton.me>
2025-06-12 15:23:56 +0200

    organising my files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import tailwindcss from '@tailwindcss/vite'
-import './App.css'
+import './styles/App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
