3e13d59560eaae44a22fc8a665411ee0f026fbae
BeshrAlkharrat <beshralkharrat@proton.me>
2025-06-21 21:51:38 +0200

    set up the routing between the pages

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router'
 import '../styles/navigationbar.css'
 function NavigationBar() {
     return(
     <>
         <div className="bg-[#3f3d56] w-100 mx-auto h-20 text-gray-200 flex items-center justify-center gap-10 " >
-            <h1>Home</h1>
-            <h1>Projects</h1>
-            <h1>Contact</h1>
+            <Link to="/About">About</Link>
+            <Link to="/">Home</Link>
+            <Link to="/Contact">Contact</Link>
+            <Link to="/Projects">Projects</Link>
         </div>
     </>)
 }
