d7ee1e9320f6b0a0195bddb2fa9682deb8e55da1
BeshrAlkharrat <beshralkharrat@proton.me>
2025-06-23 11:26:58 +0200

    edited the design of the navigation bar and added a hover effect for the button while making all of the button interractive

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,16 +3,18 @@ import '../styles/tailwind.css'
 function NavigationBar() {
     return(
     <div className='flex justify-center m-10'>
-        <div className="bg-[#3f3d56] w-85 mx-auto my-5 h-15 text-gray-200 flex items-center justify-center gap-7 rounded-full shadow-xl" >
+        <div className="bg-linear-to-r from-[hsl(214,75%,50%)] to-[hsl(214,86%,67%)] w-85 mx-auto my-5 h-15 text-gray-200 flex items-center justify-center gap-7 rounded-full shadow-xl" >
             <Link to="/About" className="hover:text-[#A26769] text-[#F4A261]">About</Link>
             <Link to="/" className="hover:text-[#4A637D] text-[#B5838D]">Home</Link>
             <Link to="/Contact" className="hover:text-[#C5A880]">Contact</Link>
             <Link to="/Projects" className="hover:text-[#C5A880]">Projects</Link>
             
         </div>
-        <div className='flex w-35 h-12 bg-linear-to-r from-[#2071DD] to-[#64A1F3] rounded-lg text-gray-200 items-center justify-center m-5 text-[1.5rem] font-bold'>
-            <Link to="/Contact">Hire Me</Link>
-        </div>
+        <Link to="/Contact">
+            <div className='flex w-35 h-12 bg-linear-to-r from-[hsl(214,75%,50%)] to-[hsl(214,86%,67%)] hover:from-[hsl(214,75%,45%)] hover:to-[hsl(214,86%,62%)] rounded-lg text-gray-200 items-center justify-center m-5 text-[1.5rem] font-bold'>
+                <p>Hire Me</p>
+            </div>
+        </Link>
     </div>)
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,17 @@ import '../styles/tailwind.css'
 function Home(){
     return(<>
     <NavigationBar />
-    <div>
-        <h1 className="font-bold text-gray-200 text-[2rem]">Hi! My name is Beshr Alkharrat, and I am a <span className="flex text-[#4286F4] text-[4.5<rem]">Front-end Developer</span></h1>
+    <div className="flex w-125">
+        <h1 className="font-bold text-gray-200 text-[2rem] mx-5">Hi! My name is Beshr Alkharrat, and I am a <span className="flex text-[#4286F4] text-[3rem] font-bold">Front-end Developer</span></h1>
     </div>
     
     <div className="flex relative">
-        <div className='flex w-35 h-12 bg-[hsl(41,100%,47%)] hover:bg-[hsl(41,100%,42%)] rounded-lg text-gray-200 items-center justify-center m-5 text-[1.5rem] font-bold'>
-            <Link to="/Contact">Hire Me</Link>
-        </div>
-        <div className="flex h-12 w-45 mt-5 bg-[#121212] outline-solid outline-gray-200 hover:outline-gray-300 justify-center items-center rounded-lg text-gray-200 ">
+        <Link to="/Contact">
+            <div className='flex w-35 h-12 bg-[hsl(41,100%,47%)] hover:bg-[hsl(41,100%,42%)] rounded-lg text-gray-200 items-center justify-center m-5 text-[1.5rem] font-bold'>
+                <p>Hire Me</p>
+            </div>
+        </Link>
+        <div className="flex h-12 w-45 mt-5 bg-[#121212] outline-solid outline-gray-200 hover:outline-gray-400 justify-center items-center rounded-lg text-gray-200 ">
             <Link to="/Projects">Download CV</Link>
         </div>
         <div>
